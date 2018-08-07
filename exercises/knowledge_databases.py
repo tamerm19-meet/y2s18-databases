f30a7f3f177ce92c7c5fcb611492fefa99233719
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 11:59:21 +0300

    queries and stuff

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -16,21 +16,39 @@ def add_article(topic, article_title, rating):
 	session.add(article_object)
 	session.commit()
 
+
 def query_all_articles():
-		knowledge = session.query(Knowledge).all()
-		return knowledge
+	article = session.query(
+		Knowledge).all()
+	return article
 
-def query_article_by_topic():
-	pass
 
-def delete_article_by_topic():
-	pass
+def query_article_by_topic(topic):
+	article = session.query(
+		Knowledge).filter_by(
+			topic = topic).first()
+	return article
+	
+
+
+def delete_article_by_topic(topic):
+	session.query(Knowledge).filter_by(
+		topic = topic).delete()
+	session.commit()
+	
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).all().delete()
+	session.commit()
+	
 
-def edit_article_rating():
-	pass
+def edit_article_rating(topic, new_rating):
+	article_object = session.query(
+		Knowledge).filer_by(
+		topic = topic).first()
+	article_object.rating = new_rating
+	session.commit()
+	
 
 add_article("mhmd", "mhmd123", 10)
-print(query_all_articles)
+print(query_all_articles())
